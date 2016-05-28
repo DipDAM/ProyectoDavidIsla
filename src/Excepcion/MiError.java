2c2e6b25cf1d850b6dd5a72139ab5393d56b0e8b
DipDAM <david.isla.p93@gmail.com>
2016-05-29 01:45:24 +0200

    Creo la clase MiError, y realizo el alta de un alumno en la base de datos mediante el uso de un objeto de la clase Alumno

---
+++ src/Excepcion/MiError.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Excepcion;
+
+/**
+ *
+ * @author David
+ */
+public class MiError extends Exception{
+    public MiError(String mensaje) {
+        super(mensaje);
+    }
+}
